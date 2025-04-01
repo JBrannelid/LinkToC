6a2d52677d5c235b2d4b7377acced9f7207c78ce
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-01 19:05:17 +0200

    Update App component to include some basic view (Calendar test view)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import Calendar from "./components/calendar/calendar";
+import CalendarTest from "./testing/CalendarTest";
 
 function App() {
   return (
     <>
-      <div className="flex">
-        <div className="flex-wrap bg-amber-300 border-2 border-cyan-100 border-r">
-          <h1 className="text-bg-secondary">Hello World</h1>
-          <Calendar
-            events={enhancedEvents}
-            accentColor="bg-green-500"
-            textColor="text-primary-text"
-            noEventsMessage="Inga schemalagda händelser idag"
-          />
-        </div>
+      <div>
+        <CalendarTest />
       </div>
     </>
   );
