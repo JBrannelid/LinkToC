6bc360f3446647b225ba72d6743ba72b3b3c63ac
Rasemouse <rasmuswenngren@gmail.com>
2025-04-07 15:26:34 +0200

    Update UserProfilePage.jsx

---
+++ src/components/ui/userPage/UserProfilePage.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+// import {BasePage} from "./BasicPage.jsx";
+
+// export const ProfilePage = ({data, className}) => {
+//     return (
+//         <BasePage className={className}>
+//             <h2>{data.name}</h2>
+
+        // </BasePage>
+    // )
+// }
