adb1e3a5c0ff5516e8d2da2e96f47fa78db066a5
Rasemouse <rasmuswenngren@gmail.com>
2025-04-07 15:26:34 +0200

    Update HorseProfilePage.jsx to workwith ImageUtils

---
+++ src/components/ui/userPage/HorseProfilePage.jsx
@@ -0,0 +1,25 @@
+// import React from "react";
+// import {BasePage} from "./BasicPage.jsx";
+// import {Tabs} from "./userPageUtils.js";
+// import {Actions} from "./userPageUtils.js";
+
+
+
+// const Header = ({data}) => { 
+// return (
+//      <h2>{data.name}</h2>
+//           <p>Breed: {data.breed}</p>
+//            <p>Age: {data.age}</p>
+//             <p>Gender: {data.gender}</p>
+// 
+// )}
+// 
+// export const HorsePage = ({data, className}) => {
+//     return (
+//         <BasePage className={className}>
+//          <Header />
+//          <Tabs />
+//          <Actions />
+//         </BasePage>
+//     )
+// }
