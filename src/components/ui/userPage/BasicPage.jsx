00bdd57b2676e72b5c5d7b1c1a2033c74da38a69
Rasemouse <rasmuswenngren@gmail.com>
2025-04-07 15:26:34 +0200

    Update BasicPage.jsx

--- src/components/ui/userPage/BasicPage.jsx
+++ src/components/ui/userPage/BasicPage.jsx
@@ -1,3 +1,10 @@
 
 //Page with a basic layout  common for all different users
 
+// import React from "react";
+
+// export const BasePage = ({children, className = ''}) => {
+//     return (
+//         <div className={` ${className}`}>{children}</div>
+//     )
+// }
