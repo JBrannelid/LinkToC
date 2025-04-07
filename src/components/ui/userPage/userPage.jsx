00a53746aa244f7f0818af2c8b4d1dde0a10d50a
Rasemouse <rasmuswenngren@gmail.com>
2025-04-07 15:26:34 +0200

    Update userPage.jsx

--- src/components/ui/userPage/userPage.jsx
+++ src/components/ui/userPage/userPage.jsx
@@ -1,16 +1,29 @@
 //This page should render the userPage
 //With different values depending on if it's a user/horse/admin
 
+//import React from "react";
+//import {UserPage} from './components/page/UserProfilePage';
+// import {HorsePage} from './components/page/HorseProfilePage';
 
-//import {UserProfilePage} from './components/page/UserProfilePage';
-//import {HorseProfilePage} from './components/page/HorseProfilePage';
+export default function ProfileContainer({ type, data }) {
+    //Determine which profile to render based on type prop
+    const renderProfile = () => {
+         switch(type) {
+             case 'user':
+                 return <UserProfilePage data={data} />;
+             case 'horse':
+                 return <HorseProfilePage data={data} />;
+             case 'admin':
+                 //Add your admin component when available
+                 return <div>Admin profile coming soon</div>;
+             default:
+                 return <div>Select a profile type</div>;
+         }
+     };
 
-// export default function () {
-//     return;
-// };
-
-// return ( 
-//  {renderHeader}
-//  {renderTabs}
-//  {renderFooter}
-// 
+     return (
+         <div className="profile-container">
+             {renderProfile()}
+         </div>
+     );
+ }
