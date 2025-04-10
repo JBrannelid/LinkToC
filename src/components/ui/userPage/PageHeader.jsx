ed8535ed68f418114d1eca2e2340a0f495d83e75
Rasemouse <rasmuswenngren@gmail.com>
2025-04-10 09:13:43 +0200

    Update PageHeader.jsx

--- src/components/ui/userPage/PageHeader.jsx
+++ src/components/ui/userPage/PageHeader.jsx
@@ -1,11 +1,13 @@
-// PageHeader.jsx
+
 import React from 'react';
 
-export const PageHeader = ({ className = "", children, id }) => {
+const PageHeader = ({ className = "", children, id }) => {
     return (
         <header className={`px-4 py-3 border-b border-gray-200 ${className}`} id={id}>
             {children}
         </header>
     );
 };
+
+export default PageHeader;
 
