c43a666122b7f515bf0add8ff9b07bff45ba3b67
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-11 22:25:49 +0200

    Added custom endpoints methods to match BE DTO's

--- src/api/services/eventService.js
+++ src/api/services/eventService.js
@@ -1,13 +1,38 @@
 import createBaseService from "../services/baseService";
 import { ENDPOINTS } from "./endPoints";
 
-// Create base service with standard CRUD operations
 const baseService = createBaseService(ENDPOINTS.EVENTS);
 
 const eventService = {
   ...baseService,
 
-  // Implement event-specific method
+  create: async (data) => {
+    // baseService will handle validation of data
+
+    const createData = {
+      title: data.title,
+      startDateTime: data.startDateTime,
+      endDateTime: data.endDateTime,
+      stableIdFk: 1, // Default value since we're not handling stable foreign key atm.
+    };
+
+    return await baseService.create(createData);
+  },
+
+  update: async (data) => {
+    const updateData = {
+      id: data.id,
+      title: data.title,
+      startDateTime: data.startDateTime,
+      endDateTime: data.endDateTime,
+      stableIdFk: data.stableIdFk || 1,
+    };
+
+    console.log("Sending update data:", JSON.stringify(updateData));
+    return await baseService.update(updateData);
+  },
+
+  // Implement event-specific method to ensure DTO formatting
 };
 
 export default eventService;
