1a791c9bb3a298586a7c7b1e1b56917478d0b1d2
MJ Eng <mj.eng.contact@gmail.com>
2025-04-14 11:50:31 +0200

    clean up functions for stableName component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,28 +5,9 @@ import Calendar from "../components/calendar/calendar";
 export default function Home() {
   //stable id
   const id = 1;
-  // const [id, setId] = useState();
-  // const { input, setInput } = useState(0);
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   setId(Number(input));
-  // };
 
   return (
     <>
-      {/* <form>
-        <label>
-          Enter Stable ID:
-          <input
-            type="number"
-            value={input}
-            onChange={(i) => setInput(i.target.valueAsNumber)}
-          />
-        </label>
-        <button type="submit">Load Stable</button>
-      </form> */}
-
       <StableName id={id} />
       <div>Important Notice!!</div>
       <div>
