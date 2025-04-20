a4e7473be9997206801f8f5941fe9cb6a267ca34
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-20 18:58:44 +0200

    Created userService with stableID filtering. Added specific functions for createUser and get user connected with a stableId

---
+++ src/api/services/userService.js
@@ -0,0 +1,27 @@
+import createBaseService from "../services/baseService";
+import { ENDPOINTS } from "./endPoints";
+
+// baseService will handle validation of data
+const baseService = createBaseService(ENDPOINTS.USERS);
+
+const userService = {
+  ...baseService,
+
+  // Get user base on a specifik stable Id
+  getStableUsers: async (stableId) => {
+    const users = await baseService.getAll();
+    // Filtering user by stable-Id should be handle by backend
+    return users.filter((user) => user.stableIdFk === stableId);
+  },
+
+  createUser: async (userData, stableId) => {
+    const createData = {
+      ...userData,
+      stableIdFk: stableId,
+    };
+
+    return await baseService.create(createData);
+  },
+};
+
+export default userService;
