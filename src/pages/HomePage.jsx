40ae14fe61d03f81be698cb685a0c949fe4def81
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-22 22:23:21 +0200

    Add HomePage component displaying the calendar section using DisplayCalendar

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DisplayCalendar from "../components/calendar/CalendarDisplay";
+
+export default function HomePage() {
+  return (
+    <div className="">
+      <section className="mb-6">
+        <DisplayCalendar />
+      </section>
+    </div>
+  );
+}
