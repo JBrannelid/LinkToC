f47aae4ef3ba226a9d4e61399b94f195c84a27db
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-22 15:55:35 +0200

    Added a basic service to get a user by stable Id and to create a user with a stable foreign key

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -1,7 +1,6 @@
 import createBaseService from "../services/baseService";
 import { ENDPOINTS } from "./endpoints";
 
-// baseService will handle validation of data
 const baseService = createBaseService(ENDPOINTS.USERS);
 
 const userService = {
@@ -10,7 +9,8 @@ const userService = {
   // Get user base on a specifik stable Id
   getStableUsers: async (stableId) => {
     const users = await baseService.getAll();
-    // Filtering user by stable-Id should be handle by backend
+
+    // Filtering user by stable-Id should be handle by backend?
     return users.filter((user) => user.stableIdFk === stableId);
   },
 
