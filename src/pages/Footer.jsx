c67edf3bfa112168cd755b2733066f015f545001
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-23 21:23:24 +0200

    Change default route for home icon that navigated to root url to homepage

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,5 +1,6 @@
 import { Home, Warehouse, User, Rabbit } from "lucide-react";
 import { Link } from "react-router";
+import { ROUTES } from "../routes/routeConstants";
 
 // Hardcoded for now, should be replaced with a dynamic variable later
 const horseId = 1;
@@ -8,7 +9,7 @@ const userId = 1;
 export default function Footer() {
   return (
     <footer className="flex items-center justify-between p-4 bg-white shadow-md">
-      <Link to={`/`}>
+      <Link to={ROUTES.HOME}>
         <Home className="w-6 h-6 text-gray-700" />
       </Link>
 
