23047d8b826d88dee848301aa3a53902cc514f68
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-23 21:17:16 +0200

    Add protected route wrapper for route configs

---
+++ src/routes/routeUtils.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ProtectedRoute from "./ProtectedRoute";
+
+// Converts a route config into a protected route by wrapping it in <ProtectedRoute>, applying stable checks if required
+export const createProtectedRoute = (routeConfig) => {
+  const { element, requiresStable, ...rest } = routeConfig;
+
+  return {
+    ...rest,
+    element: (
+      <ProtectedRoute requiresStable={requiresStable}>{element}</ProtectedRoute>
+    ),
+  };
+};
+
+export const createProtectedRoutes = (routes) => {
+  return routes.map(createProtectedRoute);
+};
