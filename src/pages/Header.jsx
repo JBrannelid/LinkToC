54040413e147642680c67edf4b4db17083d48b59
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-23 22:18:33 +0200

    Added a link to settings icon

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,13 +1,17 @@
 import { Settings, Mail } from "lucide-react";
 import StableName from "../components/home/StableName";
 import { useAppContext } from "../context/AppContext";
+import { Link } from "react-router";
+import { ROUTES } from "../routes/routeConstants";
 
 export default function Header() {
   const { currentStable } = useAppContext();
 
   return (
     <header className="flex items-center justify-between p-4 bg-white shadow-md">
-      <Settings className="w-6 h-6 text-gray-700" />
+      <Link to={ROUTES.SETTINGS} aria-label="Inställningar">
+        <Settings className="w-6 h-6 text-gray-700 hover:text-gray-900" />
+      </Link>
 
       <div className="text-sm font-light">
         <StableName currentStableId={currentStable.id} />
