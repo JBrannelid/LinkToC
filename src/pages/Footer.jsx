85800bcd5e17dcc9103a7ea69d68d42baf3882ab
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-23 17:31:17 +0200

    Added default placeholder links

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,12 +1,28 @@
 import { Home, Warehouse, User, Rabbit } from "lucide-react";
+import { Link } from "react-router";
+
+// Hardcoded for now, should be replaced with a dynamic variable later
+const horseId = 1;
+const userId = 1;
 
 export default function Footer() {
   return (
     <footer className="flex items-center justify-between p-4 bg-white shadow-md">
-      <Home className="w-6 h-6 text-gray-700" />
-      <Warehouse className="w-6 h-6 text-gray-700" />
-      <Rabbit className="w-6 h-6 text-gray-700" />
-      <User className="w-6 h-6 text-gray-700" />
+      <Link to={`/`}>
+        <Home className="w-6 h-6 text-gray-700" />
+      </Link>
+
+      <Link to={`/stable/${userId}`}>
+        <Warehouse className="w-6 h-6 text-gray-700" />
+      </Link>
+
+      <Link to={`/horsepage/${horseId}`}>
+        <Rabbit className="w-6 h-6 text-gray-700" />
+      </Link>
+
+      <Link to={`/userpage/${userId}`}>
+        <User className="w-6 h-6 text-gray-700" />
+      </Link>
     </footer>
   );
 }
