be307f217afcd0ed2fb10d414b668f632bae4ce2
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-23 21:07:25 +0200

    move out route builder to own configuration folder - Rotes

--- src/main.jsx
+++ src/main.jsx
@@ -1,64 +1,11 @@
 import "./index.css";
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import { Navigate } from "react-router";
-import App from "./App.jsx";
-import ErrorPage from "./pages/ErrorPage.jsx";
-import HomePage from "./pages/HomePage.jsx";
-import ProfileTester from "./pages/ProfileTester.jsx";
+import { RouterProvider } from "react-router";
+import { router } from "./routes/index.jsx";
 import { AppProvider } from "./context/AppContext";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import ProtectedRoute from "./routes/ProtectedRoute.jsx";
-import LoginForm from "./pages/LoginPage.jsx";
-import RegistrationPage from "./pages/RegistrationPage.jsx";
-import StableSelectionPage from "./pages/StableSelectionPage.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Navigate to="/login" replace />,
-      },
-      {
-        path: "login",
-        element: <LoginForm />,
-      },
-      {
-        path: "register",
-        element: <RegistrationPage />,
-      },
-      {
-        path: "select-stable",
-        element: (
-          <ProtectedRoute>
-            <StableSelectionPage />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "home",
-        element: (
-          <ProtectedRoute requiresStable={true}>
-            <HomePage />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "horsepage/:horseId",
-        element: (
-          <ProtectedRoute requiresStable={true}>
-            <ProfileTester />
-          </ProtectedRoute>
-        ),
-      },
-    ],
-  },
-]);
+import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
