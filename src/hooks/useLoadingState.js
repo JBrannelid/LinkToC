ec90246f05fd4c6941b0845c6b8b60c3203fe14d
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-27 20:24:56 +0200

    Update component with a operationtyp for CRUD-operations

--- src/hooks/useLoadingState.js
+++ src/hooks/useLoadingState.js
@@ -1,6 +1,22 @@
-export const useLoadingState = (isLoading, loadingMessage = "Loading...") => {
+export const useLoadingState = (isLoading, operationType = "default") => {
+  const getLoadingMessage = () => {
+    switch (operationType) {
+      case "fetch":
+        return "Laddar hem data, vänligen vänta...";
+      case "create":
+        return "Skapar ny data, vänligen vänta...";
+      case "update":
+        return "Uppdaterar, vänligen vänta...";
+      case "delete":
+        return "Tar bort, vänligen vänta...";
+      default:
+        return "Snart är vi färdiga, vänligen vänta...";
+    }
+  };
+
   return {
     isLoading,
-    loadingMessage,
+    operationType,
+    getMessage: getLoadingMessage,
   };
 };
