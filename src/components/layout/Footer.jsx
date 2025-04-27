e4a5a908ba9c051dbda8b31fd77f7a741efa2aef
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-27 12:36:05 +0200

    Added a z index of 100 to overlay navigationbar over all other components

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import NavigationBar from "../layout/NavigationBar";
 
 export default function Footer() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-10">
+    <nav className="fixed bottom-0 left-0 right-0 z-100">
       <NavigationBar />
     </nav>
   );
