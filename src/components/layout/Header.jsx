fc0525470209027cc73791e92d93236c46a564ab
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-29 13:59:31 +0200

    Reset header to main remote commit sense bg color logic is removed til HeaderContainer

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,29 +1,21 @@
 import { useAppContext } from "../../context/AppContext";
-import { Link, useLocation } from "react-router";
+import { Link } from "react-router";
 import { ROUTES } from "../../routes/routeConstants";
 import SettingIcon from "../../assets/icons/SettingIcon";
 import NotificationIcon from "../../assets/icons/NotificationIcon";
-import ModalHeader from "../layout/ModalHeader";
 
 export default function Header() {
   const { currentStable } = useAppContext();
-  const location = useLocation();
-
-  const setBgToPrimaryLight = location.pathname === ROUTES.SETTINGS;
-
-  const backgroundClass = setBgToPrimaryLight ? "bg-primary-light" : "";
 
   return (
-    <ModalHeader className={`p-4 ${backgroundClass}`}>
-      <div className="flex items-center justify-between w-full mt-4">
-        <Link to={ROUTES.SETTINGS} aria-label="Link to setting page">
-          <SettingIcon strokeWidth={6} className="w-6 h-6 text-primary" />
-        </Link>
+    <header className="flex items-center justify-between p-4">
+      <Link to={ROUTES.SETTINGS} aria-label="Inställningar">
+        <SettingIcon strokeWidth={9} className="w-7 h-7 text-olive-500" />
+      </Link>
 
-        <Link to={ROUTES.NOTOFICATIONS} aria-label="Link to notification page">
-          <NotificationIcon className="w-6 h-6 text-primary" />
-        </Link>
-      </div>
-    </ModalHeader>
+      <Link to="/notifications" aria-label="Notifikationer">
+        <NotificationIcon className="w-6 h-6 text-olive-500" />
+      </Link>
+    </header>
   );
 }
