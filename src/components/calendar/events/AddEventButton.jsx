526c10fcf31ce1841e48a1a29c7757cba621db6e
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-29 21:30:14 +0200

    Made changes to css styles type

--- src/components/calendar/events/AddEventButton.jsx
+++ src/components/calendar/events/AddEventButton.jsx
@@ -7,12 +7,13 @@ const AddEventButton = ({ onClick }) => {
     <div className="fixed bottom-25 right-4">
       <Button
         type="primary"
-        size="small"
-        className="rounded-full w-10 h-10"
+        variant="icon"
+        size="medium"
+        className="!bg-primary !rounded-full"
         aria-label="Add new event button"
         onClick={onClick}
       >
-        <PlusIcon className="h-5 w-6 bg-primary text-white " strokeWidth={3} />
+        <PlusIcon className="h-6 w-6 text-white" strokeWidth={3} />
       </Button>
     </div>
   );
