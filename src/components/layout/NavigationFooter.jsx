a062d40e66ee73dbe2b8b506c4b415d699bf23b4
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-29 08:34:39 +0200

    Change name to adapt what the component is responsible for

---
+++ src/components/layout/NavigationFooter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NavigationBar from "./NavigationBar";
+
+export default function NavigationFooter() {
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 z-100">
+      <NavigationBar />
+    </nav>
+  );
+}
