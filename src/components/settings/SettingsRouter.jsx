6b7fdc6bb0b79b2e5c9eaf73b6f0f84e462d84b3
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-04-30 22:54:00 +0200

    Create SettingsRouter to render different settings pages based on user role

---
+++ src/components/settings/SettingsRouter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAppContext } from "../../context/AppContext";
+import { ROLES } from "../../utils/userUtils";
+import UserSettingsPage from "../../pages/UserSettingsPage";
+import AdminSettingsPage from "../../pages/AdminSettingsPage";
+
+// Decide routing based on the user's role in the current stable
+const SettingsRouter = () => {
+  const { getCurrentStableRole } = useAppContext();
+
+  const currentRole = getCurrentStableRole();
+
+  // Pass admin routes
+  if (currentRole === ROLES.USER_ADMIN || currentRole === ROLES.MASTER_ADMIN) {
+    return <AdminSettingsPage />;
+  }
+
+  // Pass user routes
+  return <UserSettingsPage />;
+};
+
+export default SettingsRouter;
