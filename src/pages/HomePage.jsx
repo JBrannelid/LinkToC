1810094591cb8a4cdefa3f7fbbd002a0e79c09d8
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-05-03 16:39:59 +0200

    Adding responsive to hompage to act as a main css container

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,11 +4,11 @@ import WallPost from "../components/posts/WallPost";
 
 export default function HomePage() {
   return (
-    <div className="h-screen pb-30">
-      <section>
+    <div className="mx-auto min-h-screen pb-20 md:pb-24 lg:pb-30 px-4 md:px-6 lg:px-8">
+      <section className="mb-6 md:mb-8">
         <DisplayCalendar />
       </section>
-      <section>
+      <section className="mx-auto mb-16 md:mb-10 lg:mb-6 md:mx-0 md:max-w-none">
         <WallPost />
       </section>
     </div>
