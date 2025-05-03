e15a24784e9caebf239717a36adc1cae5bf35fe2
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-05-03 22:04:56 +0200

    Remove unused import and stable context

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,9 @@
-import { useAppContext } from "../../context/AppContext";
 import { Link } from "react-router";
 import { ROUTES } from "../../routes/routeConstants";
 import SettingIcon from "../../assets/icons/SettingIcon";
 import NotificationIcon from "../../assets/icons/NotificationIcon";
 
 export default function Header() {
-  const { currentStable } = useAppContext();
-
   return (
     <header className="flex items-center justify-between p-4">
       <Link to={ROUTES.SETTINGS} aria-label="Inställningar">
