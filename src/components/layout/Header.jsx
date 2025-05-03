ba5ac8e4f743d4a0fe16ff8ff3ec306e66999310
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-05-03 22:10:41 +0200

    Improve layout and css

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,13 +5,16 @@ import NotificationIcon from "../../assets/icons/NotificationIcon";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between p-4">
+    <header className="flex items-center justify-between p-4 md:justify-start md:pt-6 md:gap-10">
       <Link to={ROUTES.SETTINGS} aria-label="Inställningar">
-        <SettingIcon strokeWidth={9} className="w-7 h-7 text-olive-500" />
+        <SettingIcon
+          strokeWidth={9}
+          className="w-6 h-6 md:w-8 md:h-8  text-primary"
+        />
       </Link>
 
       <Link to="/notifications" aria-label="Notifikationer">
-        <NotificationIcon className="w-6 h-6 text-olive-500" />
+        <NotificationIcon className="w-6 h-6 md:w-8 md:h-8 text-primary" />
       </Link>
     </header>
   );
