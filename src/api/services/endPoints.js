1649925ca93ffd22d484017accafb3825f76e3e5
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-05-07 09:33:57 +0200

    Update stable endpoint

--- src/api/services/endPoints.js
+++ src/api/services/endPoints.js
@@ -1,6 +1,6 @@
 export const ENDPOINTS = {
   EVENTS: "/api/calendar-event",
-  STABLE: "/api/stable/search",
+  STABLE: "/api/stable",
   STABLE_REQUESTS: "/api/get-stable-join-request-by-stable",
   STABLE_INVITES: "/api/get-stable-invite-by-stable",
   STABLEPOST: "api/stable-post",
