39f6ad96ded303496fc05a15bd96e3e02487c39b
Rasemouse <rasmuswenngren@gmail.com>
2025-05-07 15:47:17 +0200

    added indexfile

---
+++ src/components/search/index.js
@@ -0,0 +1,10 @@
+import SearchProvider from "./SearchProvider.jsx";
+import SearchActions from "./components/SearchActions.jsx";
+import SearchBar from "./components/SearchBar.jsx";
+import SearchResults from "./components/SearchResults.jsx";
+import SearchField from "./components/SearchField.jsx";
+
+
+export {SearchBar,SearchResults,SearchActions,SearchField,SearchProvider};
+
+export default SearchFunction;
