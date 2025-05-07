df5beed0946bdfd6cea49c17150d50c21080a21b
MJ Eng <mj.eng.contact@gmail.com>
2025-05-07 12:48:18 +0200

    add getById function

--- src/api/services/stableService.js
+++ src/api/services/stableService.js
@@ -6,14 +6,16 @@ const baseService = createBaseService(ENDPOINTS.STABLE);
 
 const stableService = {
   ...baseService,
-  
-  
+
   createWithWallPost: async (data) => {
     if (!data || !data.name) {
       throw new Error("Stable name is required");
     }
 
-    return await axiosConfig.post(`${ENDPOINTS.STABLE}/create-with-wall-post`, data);
+    return await axiosConfig.post(
+      `${ENDPOINTS.STABLE}/create-with-wall-post`,
+      data
+    );
   },
 };
 
--- src/hooks/useStableData.js
+++ src/hooks/useStableData.js
@@ -4,6 +4,7 @@ import { useLoadingState } from "./useLoadingState";
 
 export function useStableData(stableId) {
   const [stables, setStables] = useState([]);
+  const [stableInfo, setStableInfo] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [operationType, setOperationType] = useState("fetch");
@@ -44,6 +45,23 @@ export function useStableData(stableId) {
 
   const loadingState = useLoadingState(loading, operationType);
 
+  const getById = useCallback(async () => {
+    setOperationType("fetch");
+    try {
+      setLoading(true);
+      setError(false);
+      const fetchStableInfo = await stableService.getById(stableId);
+      setStableInfo(fetchStableInfo.value);
+      return true;
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+      return false;
+    } finally {
+      setLoading(false);
+    }
+  }, [stableId]);
+
   return {
     stables,
     stableId,
@@ -51,6 +69,8 @@ export function useStableData(stableId) {
     loadingState,
     fetchAndUpdateStables,
     getStableById,
+    getById,
+    stableInfo,
   };
 }
 
