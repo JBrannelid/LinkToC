fbc92cca159397fe84e495f8849af0d84b8e5656
MJ Eng <mj.eng.contact@gmail.com>
2025-05-09 13:17:00 +0200

    create stableHorse component for its page

---
+++ src/components/stableHorse/StableHorseList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function StableHorseList() {
+  return <div>StableHorseList</div>;
+}
---
+++ src/pages/StableHorsePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ModalHeader from "../components/layout/ModalHeader";
+import StableInfo from "../components/layout/StableInfo";
+import ProfileCard from "../components/ui/card/ProfileCard";
+function StableHorsePage() {
+  return (
+    <>
+      <StableInfo />
+      <div>
+        {/* search */}
+        {/* stable horse list */}
+      </div>
+    </>
+  );
+}
+export default StableHorsePage;
