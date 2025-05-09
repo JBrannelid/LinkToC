46fae87594cd81ffa5fc3bb0349c80b29d9cd55e
MJ Eng <mj.eng.contact@gmail.com>
2025-05-09 13:16:00 +0200

    fix visual layout

--- src/components/layout/StableInfo.jsx
+++ src/components/layout/StableInfo.jsx
@@ -4,6 +4,11 @@ import { useEffect } from "react";
 import { useStableData } from "../../hooks/useStableData";
 import LoadingSpinner from "../ui/LoadingSpinner";
 import ModalHeader from "./ModalHeader";
+import HorseRidingIcon from "../../assets/icons/HorseRidingIcon";
+import HorseFaceIcon from "../../assets/icons/HorseFaceIcon";
+import HorseBoxIcon from "../../assets/icons/HorseBoxIcon";
+import LocationPinIcon from "../../assets/icons/LocationPinIcon";
+import PeopleIcon from "../../assets/icons/PeopleIcon";
 
 export default function StableInfo() {
   const { currentStable } = useAppContext();
@@ -32,13 +37,7 @@ export default function StableInfo() {
     return <p>Error:{error.message}</p>;
   }
 
-  {
-    /* stable info, green 
-            Address, Sum of Horses, Stable for? e.g. Ridskola, Total of members, total Boxes in stable
-
-            Address, BoxCount
-        */
-  }
+  //  Lack several data!!!
   return (
     <>
       {stableInfo ? (
@@ -48,8 +47,54 @@ export default function StableInfo() {
             <ModalHeader title={stableInfo.name} />
 
             {/* stable info body */}
-            <ol>
-              <li>{stableInfo.type}</li>
+            <ol className="pb-3">
+              <li className="m-1 mb-1">
+                <p className="flex m-0.5">
+                  <LocationPinIcon
+                    className="w-5 h-5 mr-1 text-primary"
+                    size={24}
+                  />
+                  Address for stable
+                </p>
+              </li>
+              <div className="grid grid-cols-2 gap-0.5">
+                <li className="m-1">
+                  <p className="flex m-0.5 items-center">
+                    <HorseFaceIcon
+                      className="w-5 h-5 mr-1 text-primary"
+                      size={24}
+                    />
+                    Number of horses
+                  </p>
+                </li>
+                <li className="m-1">
+                  <p className="flex m-0.5 items-center">
+                    <HorseRidingIcon
+                      className="w-5 h-5 mr-1 text-primary mt-0.5"
+                      size={24}
+                    />
+                    {stableInfo.type}
+                  </p>
+                </li>
+                <li className="m-1">
+                  <p className="flex m-0.5 items-center">
+                    <PeopleIcon
+                      className="w-5 h-4 mr-1 text-primary"
+                      size={24}
+                    />
+                    Total members
+                  </p>
+                </li>
+                <li className="m-1">
+                  <p className="flex m-0.5 items-center">
+                    <HorseBoxIcon
+                      className="w-5 h-5 mr-1 text-primary mt-0.5"
+                      size={24}
+                    />
+                    Total Boxes
+                  </p>
+                </li>
+              </div>
             </ol>
           </div>
         </div>
