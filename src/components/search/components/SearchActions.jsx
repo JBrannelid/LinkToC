f8bf78f3f6234b4e3d1a80ae72457ae80910e5ef
Rasemouse <rasmuswenngren@gmail.com>
2025-05-09 08:13:56 +0200

    removed redundant code

--- src/components/search/components/SearchActions.jsx
+++ src/components/search/components/SearchActions.jsx
@@ -3,58 +3,23 @@ import {useSearch} from "../../../context/searchContext";
 import Button from '../../ui/Button'
 
 const SearchActions = ({
-                           onAction,
                            onCancel,
-                           actionButtonClassName = '',
                            cancelButtonClassName = '',
                            actionsContainerClassName = ''
                        }) => {
     const {
         config,
-        selectedItem,
-        selectedItems,
         loading,
-        loadingState,
-        selectionMode
     } = useSearch();
-
-    const isActionEnabled = selectionMode === 'multiple' ? selectedItems.length > 0 : selectedItem !== null;
-
-    const handleActionClick = () => {
-        if (onAction) {
-            if (selectionMode === 'multiple') {
-                onAction(selectedItems);
-            } else {
-                onAction(selectedItem);
-            }
-        }
-    };
+    
     const handleCancelClick = () => {
         if (onCancel) {
             onCancel();
         }
     };
-    const actionButtonRef = useRef(null);
 
     return (
         <div className={`space-y-3 ${actionsContainerClassName}`}>
-            {/* Primary action button (Join, Select, etc.) */}
-            {isActionEnabled && (
-                <Button
-                    ref={actionButtonRef}
-                    type="primary"
-                    onClick={handleActionClick}
-                    loading={loading && loadingState?.operationType === 'update'}
-                    disabled={loading || !isActionEnabled}
-                    className={`w-full ${actionButtonClassName}`}
-                    aria-busy={loading && loadingState?.operationType === 'update'}
-                    data-action-button="primary"
-                >
-                    {loading && loadingState?.operationType === 'update'
-                        ? loadingState.getMessage()
-                        : config?.actionButtonText || 'Join'}
-                </Button>
-            )}
 
             {/* Cancel button */}
             <Button
@@ -64,7 +29,7 @@ const SearchActions = ({
                 className={`w-full ${cancelButtonClassName}`}
                 data-action-button="secondary"
             >
-                {config?.cancelButtonText || 'Avbryt'}
+                {config?.cancelButtonText || 'Cancel'}
             </Button>
         </div>
     );
