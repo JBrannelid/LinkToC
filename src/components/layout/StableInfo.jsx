bd601d96064eb892fcc9a932737d34eca6c51527
MJ Eng <mj.eng.contact@gmail.com>
2025-05-11 00:02:28 +0200

    Improve visual, closer to Mockup

--- src/components/layout/StableInfo.jsx
+++ src/components/layout/StableInfo.jsx
@@ -51,26 +51,26 @@ export default function StableInfo() {
               <li className="m-1 mb-1">
                 <p className="flex m-0.5">
                   <LocationPinIcon
-                    className="w-5 h-5 mr-1 text-primary"
+                    className="w-5 h-5 mr-2 text-primary"
                     size={24}
                   />
-                  Address for stable
+                  {stableInfo.address}
                 </p>
               </li>
               <div className="grid grid-cols-2 gap-0.5">
                 <li className="m-1">
                   <p className="flex m-0.5 items-center">
                     <HorseFaceIcon
-                      className="w-5 h-5 mr-1 text-primary"
+                      className="w-5 h-5 mr-3 text-primary"
                       size={24}
                     />
-                    Number of horses
+                    Hästar {stableInfo.horseCount}
                   </p>
                 </li>
                 <li className="m-1">
                   <p className="flex m-0.5 items-center">
                     <HorseRidingIcon
-                      className="w-5 h-5 mr-1 text-primary mt-0.5"
+                      className="w-5 h-5 mr-3 text-primary mt-0.5"
                       size={24}
                     />
                     {stableInfo.type}
@@ -79,19 +79,19 @@ export default function StableInfo() {
                 <li className="m-1">
                   <p className="flex m-0.5 items-center">
                     <PeopleIcon
-                      className="w-5 h-4 mr-1 text-primary"
+                      className="w-5 h-4 mr-3 text-primary"
                       size={24}
                     />
-                    Total members
+                    Medlemar {stableInfo.memberCount}
                   </p>
                 </li>
                 <li className="m-1">
                   <p className="flex m-0.5 items-center">
                     <HorseBoxIcon
-                      className="w-5 h-5 mr-1 text-primary mt-0.5"
+                      className="w-5 h-5 mr-3 text-primary mt-0.5"
                       size={24}
                     />
-                    Total Boxes
+                    Boxar {stableInfo.boxCount}
                   </p>
                 </li>
               </div>
