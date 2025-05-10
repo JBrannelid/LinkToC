6c0a23ac09be14be622b92eea0f22498c2b7f514
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-05-10 19:17:03 +0200

    Update header title to english

--- src/components/forms/UserProfileForm.jsx
+++ src/components/forms/UserProfileForm.jsx
@@ -138,7 +138,7 @@ const UserProfileForm = ({ onClose, onSuccess, userData: initialUserData }) => {
     <div className="fixed inset-0 z-50 bg-white md:bg-black/20 md:backdrop-grayscale shadow-md flex flex-col md:items-center md:justify-center">
       <div className="w-full h-full md:max-h-8/10 md:w-xl overflow-y-auto bg-white shadow-md rounded flex flex-col relative">
         <ModalHeader
-          title="Redigera profil"
+          title="Edit profil"
           showCloseBtn={true}
           onCloseClick={onClose}
           className="bg-primary-light"
