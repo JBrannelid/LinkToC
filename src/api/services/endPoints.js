cfc625dafd94b104edc4c5c10658f4cbfb21013a
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-05-10 12:03:38 +0200

    Update userEXTRACT_USER_ROLES base uri

--- src/api/services/endPoints.js
+++ src/api/services/endPoints.js
@@ -10,5 +10,5 @@ export const ENDPOINTS = {
   WALLPOST: "api/wallpost",
   AUTH: "/api/auth",
   STABLEPOSTBYID: "/api/stable-post-by-stable-id/",
-  EXTRACT_USER_ROLES: "/api/userstables/",
+  EXTRACT_USER_ROLES: "/api/user-stables/",
 };
