057e50276733703cb32895bfaad234da71491259
MJ Eng <mj.eng.contact@gmail.com>
2025-05-12 15:38:31 +0200

    add StableHorseCard

--- src/pages/StableHorsePage.jsx
+++ src/pages/StableHorsePage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import ModalHeader from "../components/layout/ModalHeader";
 import StableInfo from "../components/layout/StableInfo";
-import ProfileCard from "../components/ui/card/ProfileCard";
+import StableHorseCard from "../components/stableHorse/StableHorseCard";
 function StableHorsePage() {
   return (
     <>
-      <StableInfo />
-      <div>
-        {/* search */}
-        {/* stable horse list */}
+      <div className="flex flex-col min-h-screen bg-background pb-20 overflow-y-hidden">
+        <StableInfo />
+        <div>
+          {/* search */}
+          <div>Search bar</div>
+          {/* stable horse list */}
+          <h2 className="text-center border-1 border-primary shadow-lg rounded-lg bg-white p-3 mx-8 my-4">
+            Min Häst
+          </h2>
+          <StableHorseCard />
+        </div>
       </div>
     </>
   );
