1d8c1dd89c442c08b645c6a5c2133d91f01b392b
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-05-12 23:45:54 +0200

    Added hide hideBackButton

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -9,10 +9,15 @@ import mockNotificationsData from "../../testing/mockNotifications.json";
 
 export default function Header() {
   const location = useLocation();
+
+  // Show back button if the current path is one of the following
   const showBackButton =
     location.pathname.includes("/manage-stable") ||
     location.pathname.includes("/stable-requests") ||
     location.pathname.includes("/notifications");
+
+  // Hide back and notification button on the following paths
+  const hideBackButton = location.pathname.includes("/stable-onboarding");
 
   // Get notification count from mock json data
   const { notifications } = mockNotificationsData;
@@ -43,20 +48,22 @@ export default function Header() {
           <CloseIcon size={30} strokeWidth={2} className="text-primary" />
         </Button>
       ) : (
-        <Link
-          to={ROUTES.NOTIFICATIONS} // Open notification
-          aria-label="Notifikationer"
-          className="absolute right-4 top-4 border-0 text-primary"
-        >
-          <div className="relative">
-            <NotificationIcon className="w-6 h-6 md:w-8 md:h-8 text-primary" />
-            {unreadCount > 0 && (
-              <span className="absolute -top-2 -right-2 w-4 h-4 md:w-5 md:h-5 bg-error-500 rounded-full text-white text-xs flex items-center justify-center">
-                {unreadCount > 9 ? "9+" : unreadCount}
-              </span>
-            )}
-          </div>
-        </Link>
+        !hideBackButton && (
+          <Link
+            to={ROUTES.NOTIFICATIONS} // Open notification
+            aria-label="Notifikationer"
+            className="absolute right-4 top-4 border-0 text-primary"
+          >
+            <div className="relative">
+              <NotificationIcon className="w-6 h-6 md:w-8 md:h-8 text-primary" />
+              {unreadCount > 0 && (
+                <span className="absolute -top-2 -right-2 w-4 h-4 md:w-5 md:h-5 bg-error-500 rounded-full text-white text-xs flex items-center justify-center">
+                  {unreadCount > 9 ? "9+" : unreadCount}
+                </span>
+              )}
+            </div>
+          </Link>
+        )
       )}
     </div>
   );
