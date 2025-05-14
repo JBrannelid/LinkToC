c65d31c27c133a653c97a3799e1f46eb92e837e1
Rasemouse <rasmuswenngren@gmail.com>
2025-05-14 15:16:41 +0200

    feat: add HorseListWithOwners wrapper component

---
+++ src/components/stableHorse/HorseListWithOwners.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import HorseList from '../settings/StableHorseList.jsx';
+
+const HorseListWithOwners = () => {
+    return <HorseList showOwners={true} />
+}
+
+export default HorseListWithOwners;
