c2815d90f8c617ab8e5e024c35078c01798ef57b
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-05-18 13:25:51 +0200

    Ingrese text size and added margin on md displays and below

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -158,6 +158,10 @@ export default function HomePage() {
 
   return (
     <div className="mt-5 md:mt-10 pb-20 md:pb-10 sm:px-1 xl:px-16 lg:px-14">
+      <div className="flex items-center justify-center mb-2 md:hidden">
+        <StableName />
+      </div>
+
       {/* Event form modal */}
       {isFormOpen && (
         <EventForm
