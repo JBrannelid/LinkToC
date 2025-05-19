ce237ec89bcf5e8fe9341de08304871be32fbe68
Rasemouse <rasmuswenngren@gmail.com>
2025-05-19 11:26:28 +0200

    feat: export HorseResultRenderer and UserResultRenderer components

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -3,9 +3,11 @@ import SearchActions from "./components/SearchActions.jsx";
 import SearchInput from "./components/SearchInput.jsx";
 import SearchResults from "./components/SearchResults.jsx";
 import SearchField from "./components/SearchField.jsx";
+import HorseResultRenderer from "./HorseResultRenderer.jsx";
+import UserResultRenderer from "./UserResultRenderer.jsx";
 
 
-export {SearchInput, SearchResults, SearchActions, SearchField, SearchProvider};
+export {SearchInput, SearchResults, SearchActions, SearchField, SearchProvider, HorseResultRenderer, UserResultRenderer};
 
 
 
