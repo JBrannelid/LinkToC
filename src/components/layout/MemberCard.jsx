e41f5d80b44d6e19039aca2f23780629cf93e1a6
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-05-22 12:04:44 +0200

    Remove unsused files and fix background and sizing for mebercards section

--- src/components/layout/MemberCard.jsx
+++ src/components/layout/MemberCard.jsx
@@ -16,11 +16,11 @@ const MemberCard = ({ member, onClick }) => {
   return (
     <Card.Container className="cursor-pointer" onClick={onClick}>
       <div className="p-2 flex flex-col bg-gradient-to-b from-primary-light to-white">
-        <div className="flex justify-center bg-background">
-          <div className="w-24 h-24 mb-2 rounded-full overflow-hidden border-2 border-primary lg:w-full lg:h-40 xl:h-45 lg:rounded-md">
+        <div className="flex justify-center bg-background ">
+          <div className="w-full h-25 mb-2 rounded-xl overflow-hidden border-2 border-primary lg:w-full lg:h-40 xl:h-45 ">
             <ProfileImage
               user={validatedUser}
-              className="w-full h-full lg:h-50 lg:w-50 lg:aspect-ratio mx-auto"
+              className="w-30 h-30 lg:h-50 lg:w-50 lg:aspect-ratio mx-auto"
               alt={`Profile picture of ${fullName}`}
               size="medium"
               fallbackUrl={null}
