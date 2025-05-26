d377b76182bec95fe6c903f414780de552e59bb8
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-05-27 00:51:59 +0200

    Alphabetize imports and improve code formatting

--- src/utils/cardUtils.js
+++ src/utils/cardUtils.js
@@ -1,34 +1,32 @@
-
-import CardContainer from '../components/ui/Card/CardContainer.jsx';
-import CardImage from '../components/ui/Card/CardImage.jsx';
-import CardHeader from '../components/ui/Card/CardHeader.jsx';
-import CardTitle from '../components/ui/Card/CardTitle.jsx';
-import CardSubtitle from '../components/ui/Card/CardSubtitle.jsx';
-import CardBody from '../components/ui/Card/CardBody.jsx';
-import CardFooter from '../components/ui/Card/CardFooter.jsx';
+import CardBody from "../components/ui/card/CardBody.jsx";
+import CardContainer from "../components/ui/card/CardContainer.jsx";
+import CardFooter from "../components/ui/card/CardFooter.jsx";
+import CardHeader from "../components/ui/card/CardHeader.jsx";
+import CardImage from "../components/ui/card/CardImage.jsx";
+import CardSubtitle from "../components/ui/card/CardSubtitle.jsx";
+import CardTitle from "../components/ui/card/CardTitle.jsx";
 
 const Card = {
-    Container: CardContainer,
-    Image: CardImage,
-    Header: CardHeader,
-    Title: CardTitle,
-    Subtitle: CardSubtitle,
-    Body: CardBody,
-    Footer: CardFooter
+  Container: CardContainer,
+  Image: CardImage,
+  Header: CardHeader,
+  Title: CardTitle,
+  Subtitle: CardSubtitle,
+  Body: CardBody,
+  Footer: CardFooter,
 };
 export default Card;
 export function classNames(...classes) {
-    return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
 }
 
 // Also export individual components
 export {
-    CardContainer,
-    CardImage,
-    CardHeader,
-    CardTitle,
-    CardSubtitle,
-    CardBody,
-    CardFooter
+  CardContainer,
+  CardImage,
+  CardHeader,
+  CardTitle,
+  CardSubtitle,
+  CardBody,
+  CardFooter,
 };
-
