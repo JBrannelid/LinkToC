c4203ccbea8e972a612723f21a2ab75df148bf38
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-05-27 17:59:14 +0200

    Add animated underline transition between user profile tabs

--- src/components/ui/userCard/UserProfileTabs.jsx
+++ src/components/ui/userCard/UserProfileTabs.jsx
@@ -1,28 +1,37 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 const UserProfileTabs = ({ activeTab, onChange }) => {
   return (
     <div className="px-1 md:px-6 py-2 mb-4">
-      <div className="flex justify-evenly px-0 sm:px-10 md:px-20">
+      <div className="flex justify-evenly px-0 sm:px-10 md:px-20 relative">
         <button
-          className={`px-4 py-1 text-lg lg:text-xl lg:py-2 font-semibold ${
-            activeTab === "info"
-              ? "text-primary border-b-2 border-primary"
-              : "text-gray"
+          className={`px-4 py-1 text-lg lg:text-xl lg:py-2 font-semibold relative ${
+            activeTab === "info" ? "text-primary" : "text-gray"
           }`}
           onClick={() => onChange("info")}
         >
           About
+          {activeTab === "info" && (
+            <motion.div
+              layoutId="underline"
+              className="absolute bottom-0 left-0 right-0 h-0.5 bg-primary"
+            />
+          )}
         </button>
         <button
-          className={`px-4 py-1 text-lg lg:text-xl lg:py-2 font-semibold ${
-            activeTab === "horses"
-              ? "text-primary border-b-2 border-primary"
-              : "text-gray"
+          className={`px-4 py-1 text-lg lg:text-xl lg:py-2 font-semibold relative ${
+            activeTab === "horses" ? "text-primary" : "text-gray"
           }`}
           onClick={() => onChange("horses")}
         >
           Horses
+          {activeTab === "horses" && (
+            <motion.div
+              layoutId="underline"
+              className="absolute bottom-0 left-0 right-0 h-0.5 bg-primary"
+            />
+          )}
         </button>
       </div>
     </div>
