919775642bd99bc9754aaf6cdf3d4112f66af7bd
Johannes Brannelid <johannes.brannelid@chasacademy.se>
2025-06-03 15:37:45 +0200

    Build project dist

--- src/components/calendar/events/AddEventButton.jsx
+++ src/components/calendar/events/AddEventButton.jsx
@@ -4,7 +4,7 @@ import Button from "../../ui/Button";
 
 const AddEventButton = ({ onClick }) => {
   return (
-    <div className="fixed bottom-24 right-4 md:static md:mt-1 md:flex md:justify-end">
+    <div className="fixed bottom-38 right-4 md:static md:mt-1 md:flex md:justify-end">
       <Button
         type="primary"
         variant="icon"
